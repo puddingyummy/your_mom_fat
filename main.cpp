36da9a704ba167fbf1173b09b9ee655e1a843b88
### <wellnum0212@gmail.com>
2024-11-22 00:08:39 +0800

    feat: add menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void menu() {
+    cout << "1. Addition" << endl;
+    cout << "2. Subtraction" << endl;
+    cout << "3. Multiplication" << endl;
+    cout << "4. Division" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter your choice: ";
+}
+
 int main() {
     cout << "Hello, world!" << endl;
 }
