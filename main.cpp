9555d7c7e6b05301f3919483de40d6f3ce2a94be
### <wellnum0212@gmail.com>
2024-11-22 00:15:29 +0800

    fix: enemy health

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 void enemy() {
     cout << "An enemy has appeared!" << endl;
+    cout << "You must defeat the enemy to continue." << endl;
+    cout << "Type 'fight' to fight the enemy." << endl;
 }
 
 int main() {
